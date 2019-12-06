d95dc3d7ad18e7d9ebc46c19af45b3ed407d6891
Jesper Noer <jesper@noerit.gl>
2019-12-06 14:42:54 +0100

    Cleanup

--- WordGuesser/Functions.fs
+++ WordGuesser/Functions.fs
@@ -7,23 +7,17 @@ let rec listToString aList =
     [] -> ""
     |h::t -> h.ToString() + listToString t
     
-let findIndex (haystack: string) (needle: string) =
-//    if Config.CASE_SENSITIVE then
-        haystack.IndexOf(needle)
-//    else
-//        haystack.ToLower().IndexOf(needle.ToLower())
-    
 let guess (name: string) (used: string list) =
     let result = [| for i in 0..name.Length-1 -> Config.HIDDEN |]
     for u in used do
         let mutable pos = 0
-        let mutable index = findIndex name u
+        let mutable index = name.IndexOf(u)
         while index >= pos do
             if index >= 0 then
                 for i in 0..u.Length-1 do
                     result.[index+i] <- u.[i]
             pos <- pos + 1
-            index <- (findIndex (name.Substring(pos)) u) + pos
+            index <- name.Substring(pos).IndexOf(u) + pos
 
     listToString (Array.toList result)
 
