e9fd929c023fc84a3fa565c97539ee35480ad8ff
Jesper Noer <jesper@noerit.gl>
2019-12-06 09:12:29 +0100

    first commit

---
+++ WordGuesser/Config.fs
@@ -0,0 +1,9 @@
+module WordGuesser.Config
+
+let HIDDEN = '-'
+let CASE_SENSITIVE = false
+let ALLOW_BLANKS = false
+let HELP = false
+let MULTIPLE = false
+let WORDS = [ "Bob"; "Alice"; "Torsten"; "Karen"; ]
+
